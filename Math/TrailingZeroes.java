c51f98d4884c1ab347776ddb1bf79800dbaf26ff
ShrutiSri1 <40277054+ShrutiSri1@users.noreply.github.com>
2021-08-02 23:40:35 +0530

    LeetCode 172. Factorial Trailing Zeroes

---
+++ Math/TrailingZeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+       int res=0;
+        int p=5;
+        while((n/p)>0){
+            res=res+(n/p);
+            p=p*5;
+        }
+        return res;
+    }
+}
