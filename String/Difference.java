9ae5e0083b6486e911566bbb80d3963b04161838
ShrutiSri1 <40277054+ShrutiSri1@users.noreply.github.com>
2021-08-04 00:30:38 +0530

    LeetCode 389. Find the Difference

---
+++ String/Difference.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int f[]=new int[26];
+        for(int i=0;i<t.length();i++){
+            f[t.charAt(i)-'a']++;
+           
+        }
+         for(int i=0;i<s.length();i++){
+          
+            f[s.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(f[i]>0)
+                return (char)(i + 'a');
+        }
+        return ' ';
+        
+    }
+}
